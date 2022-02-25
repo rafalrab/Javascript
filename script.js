7df0511b8b4ba5ada1025a57a9d0ffa63ffdc0cd
rafal <rafalrab@o2.pl>
2022-02-25 13:40:29 +0100

    assignments + scripts added

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+let first = "jonas";
+let firstNamePerson;
+let first_name_person
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+//better
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+//worse
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
