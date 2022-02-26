06dadcc8ed5884496815fb35b17446a066677530
rafalrab <63006574+rafalrab@users.noreply.github.com>
2022-02-26 23:05:44 +0100

    Add files via upload
    var/let/const

--- assignments.js
+++ assignments.js
@@ -1,9 +1,15 @@
-let country, continent, population;
 
-country = 'Poland';
-continent = 'Europe';
-population = 39;
+const country = 'Poland';
+const continent = 'Europe';
+let population = 39;
+let language;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+language = 'Polish';
+
+console.log(isIsland);
+console.log(language);
--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// //console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Matilda";
-let first = "jonas";
-let firstNamePerson;
-let first_name_person
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Matilda";
+// let first = "jonas";
+// let firstNamePerson;
+// let first_name_person
+// console.log(firstName);
+// console.log(firstName);
+// // console.log(firstName);
 
-let PI = 3.1415;
-//better
-let myFirstJob = 'Coder';
-let myCurrentJob = 'Teacher';
-//worse
-let job1 = 'programmer';
-let job2 = 'teacher';
 
-console.log(myFirstJob);
+// // variable name conevntions
+// let PI = 3.1415;
+// //better
+// let myFirstJob = 'Coder';
+// let myCurrentJob = 'Teacher';
+// //worse
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+//console.log(typeof true);
+//console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+//const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
