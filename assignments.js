8074911e8a61b9d8f9f8258cda2cad75949aabaf
rafalrab <63006574+rafalrab@users.noreply.github.com>
2022-02-27 21:32:48 +0100

    basic operators + assignments

--- assignments.js
+++ assignments.js
@@ -13,3 +13,18 @@ language = 'Polish';
 
 console.log(isIsland);
 console.log(language);
+
+let halfPopulation = population / 2;
+
+population += 1;
+console.log(halfPopulation);
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+let description = country + ' ' + "is in " + continent + " and its " + population + " milion people speak " + language;
+
+let description1 = '${country} is in ${continent}';
+console.log(description);
+console.log(description1);
+
--- script.js
+++ script.js
@@ -44,16 +44,32 @@
 
 // console.log(typeof null);
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1991;
+// const birthYear = 1991;
 //birthYear = 1990;
 
 //const job;
 
-var job = 'programmer';
-job = 'teacher';
+// var job = 'programmer';
+// job = 'teacher';
 
-lastName = 'Schmedtmann';
-console.log(lastName);
+// lastName = 'Schmedtmann';
+// console.log(lastName);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 do potęgi 3 co jest rowne 2 * 2 * 2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10
+// console.log(x);
