13068b3fce210ebc2743c0faced9e9c35a9a347c
rafalrab <63006574+rafalrab@users.noreply.github.com>
2022-04-01 23:09:34 +0200

    arrow functions

--- script.js
+++ script.js
@@ -9,49 +9,80 @@
 // const interface = 'Audio';
 //const private = 543;
 
-function logger() {
-    console.log('My name is Jonas');
-}
+// function logger() {
+//     console.log('My name is Jonas');
+// }
 
-// calling/running/invoking a function
-logger();
-logger();
-logger();
+// // calling/running/invoking a function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function addNumber(a, b) {
+//     const c = a + b;
+//     return c;
+// }
+
+// console.log(addNumber(2, 4));
+// let sumNumber = (addNumber(2,8));
+// console.log(sumNumber);
+
+// // that was function declaration
+
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+// function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+// ARROW FUNCTION
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+
+//     //return retirement;
+
+//     return `${firstName} retires in ${retirement} years`
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
     return juice;
 }
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-console.log(fruitProcessor(5, 0));
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-function addNumber(a, b) {
-    const c = a + b;
-    return c;
-}
-
-console.log(addNumber(2, 4));
-let sumNumber = (addNumber(2,8));
-console.log(sumNumber);
-
-// that was function declaration
-
-function calcAge1(birthYear) {
-    return 2037 - birthYear;
-}
-
-const age1 = calcAge1(1991);
-console.log(age1);
-// function expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
-
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+console.log(fruitProcessor(2 ,3));
